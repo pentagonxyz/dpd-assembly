680635b0b049c58cdd97afd97a3383208bcc15c5
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-03 15:09:55 -0700

    Update comments in Repository test interface

--- test/Repository.t.sol
+++ test/Repository.t.sol
@@ -73,7 +73,10 @@ interface Repository {
     /// @notice Given a DPD id, return its updater address.
     function updaters(uint256) external view returns (address);
 
-    /// @notice Given a CID, owner address, and updater address, initialize a new DPD.
+    /// @notice Given a DPD id, return its current version.
+    function versions(uint256) external view returns (uint256);
+
+    /// @notice Given a DPD id, CID, owner address, and updater address, initialize a new DPD.
     function addDpd(
         uint256,
         bytes32,
@@ -94,12 +97,12 @@ interface Repository {
     /// @notice Event emitted when a new DPD is added to the repository.
     event DPDAdded(uint256 indexed dpdId, address owner, address updater, bytes32 cid);
 
-    /// @notice Event emitted when a new DPD is added to the repository.
+    /// @notice Event emitted when a DPD is updated.
     event DPDUpdated(uint256 indexed dpdId, bytes32 cid);
 
     /// @notice Event emitted when a DPD's owner is changed.
-    event DPDOwnerUpdated(uint256 indexed dpdId, address newOwner);
+    event DPDOwnerChanged(uint256 indexed dpdId, address newOwner);
 
     /// @notice Event emitted when a DPD's upgrader is changed.
-    event DPDUpdaterUpdated(uint256 indexed dpdId, address newUpdater);
+    event DPDUpdaterChanged(uint256 indexed dpdId, address newUpdater);
 }
